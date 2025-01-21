ba1e289e6183a45623bfc4891366a5a64c8790de
junyeong <gowjr207@gmail.com>
2025-01-21 15:57:22 +0900

    feat: make row navBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,16 +6,18 @@ const NavBar = () => {
   return (
       <BrowserRouter>
       <nav className={styles.navContainer}>
-        <ul className={styles.navList}>
-          <li className={styles.navItem}><Link to="/">Home</Link></li>
-          <li className={styles.navItem}><Link to="/mercury">Mercury</Link></li>
-          <li className={styles.navItem}><Link to="/venus">Venus</Link></li>
-          <li className={styles.navItem}><Link to="/earth">Earth</Link></li>
-          <li className={styles.navItem}><Link to="/mars">Mars</Link></li>
-          <li className={styles.navItem}><Link to="/jupiter">Jupiter</Link></li>
-          <li className={styles.navItem}><Link to="/saturn">Saturn</Link></li>
-          <li className={styles.navItem}><Link to="/uranus">Uranus</Link></li>
-          <li className={styles.navItem}><Link to="/neptune">Neptune</Link></li>
+        <div className={styles.navbar_logo}>
+          <Link to="/">Home</Link>
+        </div>
+        <ul className={styles.navbar_ul}>
+          <li className={styles.navbar_li}><Link to="/mercury">Mercury</Link></li>
+          <li className={styles.navbar_li}><Link to="/venus">Venus</Link></li>
+          <li className={styles.navbar_li}><Link to="/earth">Earth</Link></li>
+          <li className={styles.navbar_li}><Link to="/mars">Mars</Link></li>
+          <li className={styles.navbar_li}><Link to="/jupiter">Jupiter</Link></li>
+          <li className={styles.navbar_li}><Link to="/saturn">Saturn</Link></li>
+          <li className={styles.navbar_li}><Link to="/uranus">Uranus</Link></li>
+          <li className={styles.navbar_li}><Link to="/neptune">Neptune</Link></li>
         </ul>
       </nav>
       </BrowserRouter>
