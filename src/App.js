159fdf3b166f5744546d14f977f2e943fc10bf08
junyeong <gowjr207@gmail.com>
2025-01-22 14:22:20 +0900

    chore: add sample images

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import NavBar from "./NavBar";
 import Image from "./Image";
 import './App.css';
 import sample from './assets/Mars_surface.png';
-import rocket from './assets/Rocket.png';
+import sample1 from './assets/SampleImage1.jpg';
+import sample2 from './assets/SampleImage2.jpg';
 import Text from "./Text";
 import Panel from "./Panel";
 
@@ -13,7 +14,7 @@ function App() {
         <NavBar />
         <Text title="Voyager's Dream" subtitle="Photo from the Cosmos"></Text>
         <Image src={sample} alt="sample"></Image>
-        <Panel image1={rocket} image2={rocket}></Panel>
+        <Panel image1={sample1} image2={sample2}></Panel>
       </div>
   );
 }
