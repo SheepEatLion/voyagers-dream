ce65550842ab23cd1bcc9d1397168fed37088628
SheepEatLion <gowjr207@gmail.com>
2025-01-30 13:55:52 +0900

    feat: fetch image

--- src/Image.js
+++ src/Image.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Image.css';
 
 const Image = ({ src, alt }) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const queryParams = new URLSearchParams({ api_key: 'DEMO_KEY'});
+
+        try {
+            const response = await fetch(`https://api.nasa.gov/planetary/apod?${queryParams}`);
+            if (!response.ok) {
+                throw new Error('Network Error');
+            }
+            const result = await response.json();
+            setData(result);
+            console.log(result.url);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => { if(data != null) {console.log('data: ', data)} }, [data]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
       <div className="image_container">
         <img src={src} alt={alt} className="responsive_image" />
